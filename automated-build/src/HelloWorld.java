5fca340bad5f33cde40a24f6c922503dfa432d05
Stefan K. Dunkler <stefan.dun@gmail.com>
2016-07-12 15:17:34 +0200

    yet another change

--- automated-build/src/HelloWorld.java
+++ automated-build/src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main (String [] args) {
-        System.out.println("Hello World!, yeah...");
+        System.out.println("Hello World!, yeah... yeeehaa");
     }
 }
